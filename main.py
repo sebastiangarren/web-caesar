f8154a823872f611f900a2ac196e3a92a335c956
SebastianG <sebastian.garren@gmail.com>
2017-09-22 16:09:27 -0500

    Encrypt Function begun

---
+++ main.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, redirect
+from caesar import rotate_string
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border_radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+        <form name="secret_maker" method="post">
+            <input type="text" name="rot">
+            <textarea value={secret} name="text"></textarea>
+        </form>
+    </body>
+</html>
+"""
+@app.route("/", methods=['POST'])
+def encrypt():
+    secret_text = request.form["text"]
+    
+    return secret_text
+
+@app.route("/")
+
+
+def index():
+    
+    return form
+
+app.run()
